0ad92d7c46335f581423ff4095b844ac09f0a487
John Tjanaka <zonecheung@gmail.com>
2012-05-19 16:01:25 -0500

    Check for formtastic-bootstrap first in the formtastic hook.

--- lib/ckeditor/hooks/formtastic.rb
+++ lib/ckeditor/hooks/formtastic.rb
@@ -1,12 +1,25 @@
-require "formtastic"
+begin
+  require "formtastic-bootstrap"
 
-class CkeditorInput
-  include ::Formtastic::Inputs::Base
-  
-  def to_html
-    input_wrapping do
-      label_html <<
-      builder.cktext_area(method, input_html_options)
+  class CkeditorInput < FormtasticBootstrap::Inputs::TextInput
+    def to_html
+      generic_input_wrapping do
+        builder.cktext_area(method, input_html_options)
+      end
+    end
+  end
+rescue LoadError => e
+  require "formtastic"
+
+  class CkeditorInput
+    include ::Formtastic::Inputs::Base
+    
+    def to_html
+      input_wrapping do
+        label_html <<
+        builder.cktext_area(method, input_html_options)
+      end
     end
   end
 end
+
